c6d1429a92312aaa1ea1bad7d08ac1eb54275694
codesiderio <72998923+codesiderio@users.noreply.github.com>
2021-11-25 09:19:14 -0300

    Add files via upload

---
+++ Site Serra Negra/js/app.js
@@ -0,0 +1,38 @@
+const getElement = (selector) => {
+  const element = document.querySelector(selector)
+
+  if (element) return element
+  throw Error(
+    `Please double check your class names, there is no ${selector} class`
+  )
+}
+
+const date = getElement('#date')
+const currentYear = new Date().getFullYear()
+date.textContent = currentYear
+
+/*
+=============== 
+Botão
+===============
+*/
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
